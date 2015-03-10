f1e42e544a5c8964cc819c2ea6b2b13df428d4ff
Tomasz Radziejewski <tomasz@radziejewski.pl>
2015-03-10 15:23:10 +0100

    Rewrite javascript file

--- www/DeviceIDPlugin.js
+++ www/DeviceIDPlugin.js
@@ -1,10 +1,12 @@
+var exec = require('cordova/exec');
+
 var DeviceIDPlugin = {
-	getDeviceID: function(successCallback){
-		cordova.exec(successCallback, false, 'DeviceIDPlugin',
-			'getDeviceID', []);
-	},
-    getUDID: function(successCallback){
-        cordova.exec(successCallback, false, 'DeviceIDPlugin',
-			'getUniqueIdentifier', []);
+    getDeviceID: function(successCallback) {
+        exec(successCallback, false, 'DeviceIDPlugin', 'getDeviceID', []);
+    },
+    getUDID: function(successCallback) {
+        exec(successCallback, false, 'DeviceIDPlugin', 'getUniqueIdentifier', []);
     }
 };
+
+module.exports = DeviceIDPlugin;
